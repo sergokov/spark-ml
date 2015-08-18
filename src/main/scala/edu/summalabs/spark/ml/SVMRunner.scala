204b3d4b78ad8db9cb383d479d2353e1995e7e51
sergey.kovalev <sergey.kovalev@altoros.com>
2015-08-18 19:35:15 +0300

    Spark SVM

--- src/main/scala/edu/summalabs/spark/ml/SVMRunner.scala
+++ src/main/scala/edu/summalabs/spark/ml/SVMRunner.scala
@@ -6,11 +6,10 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.mllib.linalg.Matrix
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
-
 import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
-import org.apache.spark.mllib.util.MLUtils
 
 /**
  * @author Sergey Kovalev.
@@ -20,9 +19,22 @@ object SVMRunner {
   val sc = new SparkContext(conf)
 
   def main(args: Array[String]): Unit = {
-    // Load training data in LIBSVM format.
-    val data = MLUtils.loadLibSVMFile(sc, "data/mllib/sample_libsvm_data.txt")
+    val features = sc.textFile("/home/lab225/data/input/ptd2_list_id.csv_tupledsc_1024a_p2i_8_1_3_5_.csv")
+    val featuresVector = features.map(line => line.split(",")).map(arr => arr.map(el => el.toDouble)).map(arr => Vectors.dense(arr))
+    val featuresMatrix: RowMatrix = new RowMatrix(featuresVector)
+    val pcaMatrix: Matrix = featuresMatrix.computePrincipalComponents(8)
+    val featuresPcaMatrix: RowMatrix = featuresMatrix.multiply(pcaMatrix)
+    val indexFeaturesPcaMatrix: RDD[(Long ,Vector)] = featuresPcaMatrix.rows.zipWithIndex().map(kyVector => (kyVector._2, kyVector._1))
 
+    val main = sc.textFile("/home/lab225/data/input/ptd2_list_id.csv_kovadesc_main.csv")
+    val labels: RDD[(Long, Int)] = main.filter(line => !line.contains("#Idx")).map(line => line.split(","))
+      .map(arr => arr.slice(0, 2))
+      .map(arr => {
+      var label = if (arr(1).equals("m")) 1 else 0
+      (arr(0).toLong, label)
+    })
+
+    val data: RDD[LabeledPoint] = labels.join(indexFeaturesPcaMatrix).map(m => {LabeledPoint(m._2._1, m._2._2)})
     // Split data into training (60%) and test (40%).
     val splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
     val training = splits(0).cache()
@@ -62,13 +74,14 @@ object SVMRunner {
     indexFeaturesPcaMatrix
   }
 
-  def loadLabels(pathToLabels:String): RDD[(Long ,Vector)] = {
+  def loadLabels(pathToLabels:String): RDD[(Long ,Int)] = {
     val features = sc.textFile(pathToLabels).cache()
-    val featuresVector = features.map(line => line.split(",")).map(arr => arr.map(el => el.toDouble)).map(arr => Vectors.dense(arr))
-    val featuresMatrix: RowMatrix = new RowMatrix(featuresVector)
-    val pcaMatrix: Matrix = featuresMatrix.computePrincipalComponents(8)
-    val featuresPcaMatrix: RowMatrix = featuresMatrix.multiply(pcaMatrix)
-    val indexFeaturesPcaMatrix: RDD[(Long ,Vector)] = featuresPcaMatrix.rows.zipWithIndex().map(kyVector => (kyVector._2, kyVector._1))
-    indexFeaturesPcaMatrix
+    features.filter(line => !line.contains("#Idx"))
+      .map(line => line.split(","))
+      .map(arr => arr.slice(0, 2))
+      .map(arr => {
+      var label = if (arr(1).equals("m")) 1 else 0
+      (arr(0).toLong, label)
+    })
   }
 }
