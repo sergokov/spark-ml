afdc0b515a1f8f5a6fa46943de58eb6d53bfbd10
sergey.kovalev <sergey.kovalev@altoros.com>
2015-09-04 18:32:24 +0300

    SVM classification refactoring

--- src/main/scala/edu/summalabs/spark/ml/DataCombiner.scala
+++ src/main/scala/edu/summalabs/spark/ml/DataCombiner.scala
@@ -5,14 +5,14 @@ import java.io.{BufferedWriter, FileWriter}
 /**
  * @author Sergey Kovalev.
  */
-object Combiner {
+object DataCombiner {
   def main(args:Array[String]):Unit = {
-    val dataSet1: Iterator[(Int, String, Int)] = scala.io.Source.fromFile("/home/lab225/data/input/ptd2_list_id.csv_tupledsc_1024a_p2i_8_1_3_5_.csv").getLines().zipWithIndex.map(t => (2, t._1, t._2))
+    val dataSet1: List[(Int, String, Int)] = scala.io.Source.fromFile("/home/lab225/data/input/ptd2_list_id.csv_tupledsc_1024a_p2i_8_1_3_5_.csv").getLines().zipWithIndex.map(t => (2, t._1, t._2)).toList
     println(dataSet1.length)
-    val dataSet2: Iterator[(Int, String, Int)] = scala.io.Source.fromFile("/home/lab225/data/input/ptd2_list_id.csv_kovadesc_main.csv").getLines().filterNot(t => t.startsWith("#Idx,")).zipWithIndex.map(t => (1, t._1, t._2))
+    val dataSet2: List[(Int, String, Int)] = scala.io.Source.fromFile("/home/lab225/data/input/ptd2_list_id.csv_kovadesc_main.csv").getLines().filterNot(t => t.startsWith("#Idx,")).zipWithIndex.map(t => (1, t._1, t._2)).toList
     println(dataSet2.length)
     val resultFile = new BufferedWriter(new FileWriter("/home/lab225/data/input/features.txt"))
-    val joinedDataSet: List[(Int, String, Int)] = (dataSet1 ++ dataSet2).toList
+    val joinedDataSet: List[(Int, String, Int)] = dataSet1 ++ dataSet2
     println(joinedDataSet.size)
     val groupedByIndex: Map[Int, List[(Int, String, Int)]] = joinedDataSet.groupBy(_._3)
     println(groupedByIndex.keys.size)
--- src/main/scala/edu/summalabs/spark/ml/SVMClassification.scala
+++ src/main/scala/edu/summalabs/spark/ml/SVMClassification.scala
@@ -15,28 +15,24 @@ import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
  * @author Sergey Kovalev.
  */
 object SVMClassification {
-  val conf = new SparkConf().setAppName("pca_analysis")
+  val conf = new SparkConf().setAppName("SVMClassification")
   val sc = new SparkContext(conf)
 
   def main(args: Array[String]): Unit = {
-    val features = sc.textFile("/home/lab225/data/input/ptd2_list_id.csv_tupledsc_1024a_p2i_8_1_3_5_.csv")
-    val featuresVector = features.map(line => line.split(",")).map(arr => arr.map(el => el.toDouble)).map(arr => Vectors.dense(arr))
+    val dataLines = sc.textFile("/home/lab225/data/input/features.txt").cache()
+    val splittedData: RDD[Array[String]] = dataLines.map(line => line.split(","))
+    val featuresVector = splittedData.map(err => err.slice(1, err.length)).map(arr => arr.map(el => el.toDouble)).map(arr => Vectors.dense(arr))
     val featuresMatrix: RowMatrix = new RowMatrix(featuresVector)
     val pcaMatrix: Matrix = featuresMatrix.computePrincipalComponents(8)
     val featuresPcaMatrix: RowMatrix = featuresMatrix.multiply(pcaMatrix)
-    val indexFeaturesPcaMatrix: RDD[(Long ,Vector)] = featuresPcaMatrix.rows.zipWithIndex().map(kyVector => (kyVector._2, kyVector._1))
+    val indexedFeaturesPcaMatrix: RDD[(Long ,Vector)] = featuresPcaMatrix.rows.zipWithIndex().map(kv => (kv._2, kv._1))
 
-    val main = sc.textFile("/home/lab225/data/input/ptd2_list_id.csv_kovadesc_main.csv")
-    val labels: RDD[(Long, Int)] = main.filter(line => !line.contains("#Idx")).map(line => line.split(","))
-      .map(arr => arr.slice(0, 2))
-      .map(arr => {
-      var label = if (arr(1).equals("m")) 1 else 0
-      (arr(0).toLong, label)
-    })
+    val labels: RDD[(Long, Int)] = splittedData.map(arr => arr.slice(0, 1)).
+      map(arr => {if (arr(0).equals("m")) 1 else 0}).zipWithIndex().map(kv => (kv._2, kv._1))
 
-    val data: RDD[LabeledPoint] = labels.join(indexFeaturesPcaMatrix).map(m => {LabeledPoint(m._2._1, m._2._2)})
+    val dataSet: RDD[LabeledPoint] = labels.join(indexedFeaturesPcaMatrix).map(m => {LabeledPoint(m._2._1, m._2._2)})
     // Split data into training (60%) and test (40%).
-    val splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
+    val splits = dataSet.randomSplit(Array(0.6, 0.4), seed = 11L)
     val training = splits(0).cache()
     val test = splits(1)
 
@@ -59,32 +55,8 @@ object SVMClassification {
 
     println("Area under ROC = " + auROC)
 
-    val MSE = scoreAndLabels.map{case(v, p) => math.pow((v - p), 2)}.mean()
-    println("training Mean Squared Error = " + MSE)
-
     // Save and load model
     model.save(sc, "myModelPath")
     val sameModel = SVMModel.load(sc, "myModelPath")
-  }
-
-  def loadFeaturesAndApplyPCA(pathToFeatures:String): RDD[(Long ,Vector)] = {
-    val features = sc.textFile(pathToFeatures).cache()
-    val featuresVector = features.map(line => line.split(",")).map(arr => arr.map(el => el.toDouble)).map(arr => Vectors.dense(arr))
-    val featuresMatrix: RowMatrix = new RowMatrix(featuresVector)
-    val pcaMatrix: Matrix = featuresMatrix.computePrincipalComponents(8)
-    val featuresPcaMatrix: RowMatrix = featuresMatrix.multiply(pcaMatrix)
-    val indexFeaturesPcaMatrix: RDD[(Long ,Vector)] = featuresPcaMatrix.rows.zipWithIndex().map(kyVector => (kyVector._2, kyVector._1))
-    indexFeaturesPcaMatrix
-  }
-
-  def loadLabels(pathToLabels:String): RDD[(Long ,Int)] = {
-    val features = sc.textFile(pathToLabels).cache()
-    features.filter(line => !line.contains("#Idx"))
-      .map(line => line.split(","))
-      .map(arr => arr.slice(0, 2))
-      .map(arr => {
-      var label = if (arr(1).equals("m")) 1 else 0
-      (arr(0).toLong, label)
-    })
   }
 }
